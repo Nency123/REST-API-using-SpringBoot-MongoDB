0ada70d730d936476752841f304697dd4076a7bb
Nency <patelnency37@gmail.com>
2023-11-23 21:09:19 -0500

    done

---
+++ src/main/java/in/projects/movies/MovieRepository.java
@@ -0,0 +1,13 @@
+package in.projects.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie,ObjectId> {
+
+    Optional<Movie> findMovieByimdbId(String imdbId);
+}
---
+++ src/main/java/in/projects/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package in.projects.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
